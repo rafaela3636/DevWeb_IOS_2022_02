cdf9a354e42bd8340f6a111c2ca1dfa850895ce0
rafaela3636 <raphaelamarthins0202@gmail.com>
2022-10-10 21:38:32 -0300

    string acessando caractere

--- Js/01_intro_javascript/03_string/main.js
+++ Js/01_intro_javascript/03_string/main.js
@@ -1,2 +1,9 @@
 console.log("gabriela sem camisa" + "mayara sem camisa" + "alunos sem cracha");
     
+// acessando um caractere de uma string//
+
+var string1 = "falta a camisa pessoasl"
+
+console.log(string1[0])
+
+console.log(string1.charAt(3))
