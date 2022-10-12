40bfeedd2d7369056f0e85c53ea33787acddd129
rafaela3636 <raphaelamarthins0202@gmail.com>
2022-10-11 21:42:39 -0300

    subidas dos arquivos

---
+++ Js/04_estruturascondicionais/main.js
@@ -0,0 +1,16 @@
+/* Primeiro exercício */
+var pao = 10 ;
+var cafe = 15 ;
+var acucar = 20 ;
+
+if(acucar<10){
+    console.log(acucar + " é o menor produto")
+} else if (cafe<10){
+    console.log(cafe + " é o menor produto")
+}else{
+    console.log(pao +" é o menor produto")
+}
+
+/* segundo exercício*/
+var 
+
