a78158e2c762ec258178e22342f734fc086007b4
Rafaela martins <raphaelamarthins0202@gmail.com>
2022-10-13 21:15:22 -0300

    subida dos arquivos

--- Js/04_estruturascondicionais/main.js
+++ Js/04_estruturascondicionais/main.js
@@ -1,16 +1,35 @@
-/* Primeiro exercício */
-var pao = 10 ;
-var cafe = 15 ;
-var acucar = 20 ;
+//Primeiro Exercício 
 
-if(acucar<10){
-    console.log(acucar + " é o menor produto")
-} else if (cafe<10){
-    console.log(cafe + " é o menor produto")
-}else{
-    console.log(pao +" é o menor produto")
+var acucar = 'R$7';
+var cafe = 'R$28';
+var pao = 'R$17';
+
+if(acucar>cafe){
+if(acucar>pao){
+console.log("R$7 pao é o menor!");
+}else
+console.log("R$28 cafe é o menor!");
+}else if(cafe>acucar){
+if(acucar>pao){
+console.log("R$17 pao é o menor!");
+}else
+console.log("R$7 acucar é o menor!");
 }
 
-/* segundo exercício*/
-var 
+// Exercicio 2
+
+var a = 2;
+var b = 1;
+var c = 3;
+if (b < a < c) console.log('1,2,3');
+
+// Exercicio 3
+
+let ano = 2020
+if (ano % 100 == 0) {
+    console.log(ano + ' Não é um ano bissexto');
+} else if (ano % 400 == 0 || ano % 4 ==0) {
+    console.log(ano + ' É um ano bissexto');
+}
+
 
