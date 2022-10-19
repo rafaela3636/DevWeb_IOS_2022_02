12d57e961fa78f8c2c557a85864c6ba13bad1ed8
rafaela3636 <raphaelamarthins0202@gmail.com>
2022-10-18 21:48:15 -0300

    array de objetos

---
+++ Js/07_arraydeobjetos/07_exercíciodeobjetos/js/main.js
@@ -0,0 +1,38 @@
+    var aluno1 = {
+        nome: 'Abigail Santos',
+        idade: 18,
+        Notas: [8.9, 9.2],
+        endereco: {
+            rua: 'Boa Esperança',
+            numero: 120,
+            cidade: 'São Paulo',
+            estado: 'SP',
+        },
+    };
+
+        var aluno2 = {
+            nome: 'Maria Dayane ',
+            idade: 18,
+            Notas: [7.9, 6.2],
+            endereco: {
+                rua: 'Bom Sucesso',
+                numero: 50,
+                cidade: 'Vila Maria',
+                estado: 'SP',
+            },
+        };
+        var aluno3 = {
+            nome: 'Antonio Carlos ',
+            idade: 18,
+            Notas: [9.9, 5.2],
+            endereco: {
+                rua: 'acuruica',
+                numero: 30,
+                cidade: 'Vila Aurora',
+                estado: 'SP',
+            },
+        };
+
+        for(var i = 0; <alunos.length; i ++){
+            console.log
+        }
