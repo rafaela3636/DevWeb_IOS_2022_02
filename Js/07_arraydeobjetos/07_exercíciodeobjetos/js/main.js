8b05dba184c55238434e6a69f2e9166ac655cc15
rafaela3636 <raphaelamarthins0202@gmail.com>
2022-10-20 21:47:24 -0300

    subidas dos arquivos

--- Js/07_arraydeobjetos/07_exercíciodeobjetos/js/main.js
+++ Js/07_arraydeobjetos/07_exercíciodeobjetos/js/main.js
@@ -1,38 +1,63 @@
-    var aluno1 = {
+    var alunos = [
+        {
         nome: 'Abigail Santos',
         idade: 18,
-        Notas: [8.9, 9.2],
+        Notas: [7, 8, 9, 6],
         endereco: {
             rua: 'Boa Esperança',
             numero: 120,
             cidade: 'São Paulo',
-            estado: 'SP',
+            estado: 'SP' },
+        Escolaridade: 'Ensino Médio',
+        sala: 30,
+        Turno: 'noite',
+        Responsável: 'Marcos andré',
         },
-    };
-
-        var aluno2 = {
+        {
             nome: 'Maria Dayane ',
-            idade: 18,
-            Notas: [7.9, 6.2],
+            idade: 20,
+            Notas: [6, 5, 7, 8],
             endereco: {
                 rua: 'Bom Sucesso',
                 numero: 50,
                 cidade: 'Vila Maria',
-                estado: 'SP',
-            },
-        };
-        var aluno3 = {
+                estado: 'SP' },
+            Escolaridade: 'Ensino Médio',
+            sala: 38,
+           Turno: 'noite',
+           Responsável: 'Carlos Abrel',
+        },
+ 
+        {
             nome: 'Antonio Carlos ',
-            idade: 18,
-            Notas: [9.9, 5.2],
+            idade: 22,
+            Notas: [7, 4, 5, 8],
             endereco: {
                 rua: 'acuruica',
                 numero: 30,
                 cidade: 'Vila Aurora',
-                estado: 'SP',
-            },
-        };
-
-        for(var i = 0; <alunos.length; i ++){
-            console.log
+                estado: 'SP' },
+            Escolaridade: 'Ensino Médio',
+            sala: 40,
+           Turno: 'noite' ,
+           Responsável: 'Gabriel Azevedo',
         }
+    ];
+
+         let soma = 0;
+         let media = 0;
+         let aprov= " ";
+
+        for(let x = 0; x < alunos.length; x++) {
+            soma = 0;
+            for( let i = 0; i < alunos[x].notas.length; i++) soma += alunos[x].notas[i];
+
+            media = soma / alunos[x].notas.length;
+            console.log(`media ${media}`);
+            
+
+           aprov = media >= 7 ? "aprovado" : "reprovado";
+           console.log(`Aluno ${aprov}`);
+
+        }
+       
---
+++ Js/08_objetosfunçoeseeventos/Exercício/js/main.js
@@ -0,0 +1,16 @@
+function calculo( a = 2, b = 4, c = -6){
+
+    //let a = 2;
+    //let b = 4;
+    //let c = -6;
+
+    const delta = b * b - 4 * a * c;
+    //if (delta > 0) {
+    const x1 = (-b + Math.sqrt(delta)) / (2 * a);
+    const x2 = (-b - Math.sqrt(delta)) / ( 2 * a);
+
+    //}
+    console.log(x1);
+    console.log(x2);
+}
+
