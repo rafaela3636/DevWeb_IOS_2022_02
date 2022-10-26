95152aabfe6ab2ec77534161baee7ee7d7834716
rafaela3636 <raphaelamarthins0202@gmail.com>
2022-10-25 21:25:17 -0300

    subida so exercicio 11

---
+++ Js/11_ManipulandooDOMParte1/Exercício/js/main.js
@@ -0,0 +1,26 @@
+window.alert('Boas-vindas');
+
+document.title = 'Sempre';
+
+var secao = document.createElement('section');
+document.body.appendChild(secao);
+let heading = document.createElement('h1');
+secao.appendChild(heading);
+heading.innerHTML = 'Felicidade!';
+var paragrafo = document.createElement('p');
+secao.appendChild(paragrafo);
+paragrafo.innerHTML = 'Amor!';
+
+var artigo = document.createElement('article');
+document.body.appendChild(artigo);
+let artigos = document.createElement('h1');
+artigo.appendChild(artigos);
+artigos.innerHTML = 'Azul!';
+var paragrafo2 = document.createElement('p');
+artigo.appendChild(paragrafo2);
+paragrafo2.innerHTML =  'Verde!';
+
+
+
+
+
