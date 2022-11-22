dadde6919691ec888a12c7b36145ad68c275fdde
rafaela3636 <raphaelamarthins0202@gmail.com>
2022-11-21 22:00:27 -0300

    subida de arquivos java

---
+++ JAVA/PrimeiroProjetoJava/src/calcular.java
@@ -0,0 +1,13 @@
+public class calcular {
+    // primeiro passo criar metodo main
+    public static void main (String[] args){
+        //vamos somar a idade de três alunos
+        //var idade1 = 25
+        int idade1 = 25;
+        int idade2 = 15;
+        int somaIdade = idade1 + idade2;
+        System.out.println("A soma da idade" +
+                " dos alunos é:" + somaIdade);
+    }
+}
+
---
+++ JAVA/PrimeiroProjetoJava/src/pipoca.java
@@ -0,0 +1,10 @@
+public class pipoca {
+    //mostrar no console a frase hello world
+    public static void main (String[] args){
+        String texto = "hello world"
+        System.out.println("texto");
+        System.out.print(45);
+        // console.log(hello word)
+
+    }
+}
