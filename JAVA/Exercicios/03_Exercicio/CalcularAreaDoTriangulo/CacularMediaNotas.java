b26b0742fbe04aac1e82a67d0d9779ce3ad789fe
rafaela3636 <raphaelamarthins0202@gmail.com>
2022-12-07 19:00:03 -0300

    subida de arquivos java

---
+++ JAVA/Exercicios/03_Exercicio/CalcularAreaDoTriangulo/CacularMediaNotas.java
@@ -0,0 +1,12 @@
+public class CacularMediaNotas {
+    public static void main (String [] args ) {
+        int aluno1 = 4;
+        int aluno2 = 5;
+        int aluno3 = 8;
+        int aluno4 = 9;
+
+        int somaNotas = (aluno1 + aluno2 + aluno3 + aluno4) / 4;
+        System.out.ptinyln ( "A média do aluno é: " + somaNotas);
+    }
+}
+
---
+++ JAVA/Exercicios/03_Exercicio/CalcularAreaDoTriangulo/CalcularAreaTriangulo.java
@@ -0,0 +1,9 @@
+public class CalcularAreaTriangulo {
+    public static void main (String [] args) {
+        int base = 40;
+        int altura = 60;
+        int calculadorArea = (base + altura) /2;
+
+        System.out.println("A área do triangulo é: "+calcularArea);
+    }
+}
